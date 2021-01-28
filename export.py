d5a00e2dfcf8f22a1c9eef8d12acb731b25af5fc
ainki <alexinkilainen@gmail.com>
2021-01-28 18:36:32 +0200

    Done

--- export.py
+++ export.py
@@ -1,18 +1,25 @@
 import json
 import os
 
+from types import SimpleNamespace
+
 foldernames= os.listdir (".mapillary/logs/")
 
-result = []
+y = {}
+hs = open("headings.txt","r+")
 
 for foldername in foldernames: # loop through all the files and folders
     with open(".mapillary/logs/"+ foldername + "/sequence_process.json") as f:
-        data = json.load(f)
-        heading = data["MAPCompassHeading"]
-        save = {
-            "file": foldername,
-            "heading": heading
-        }
-        print(json.dumps(save))
+            data = json.load(f, object_hook=lambda d: SimpleNamespace(**d))
+            heading = data.MAPCompassHeading.TrueHeading
+            save = {
+                "file": foldername,
+                "heading": heading
+            }
+    
+            y = json.dumps(save)
+            print(y)
 
-print(result)
+            hs.write(y + "\n")
+            
+hs.close() 
