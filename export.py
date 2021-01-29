a4907b04ae5bae1304d8252ecf183f3a783ae557
ainki <alexinkilainen@gmail.com>
2021-01-29 09:47:03 +0200

    Creates a new text file

--- export.py
+++ export.py
@@ -6,7 +6,7 @@ from types import SimpleNamespace
 foldernames= os.listdir (".mapillary/logs/")
 
 y = {}
-hs = open("headings.txt","r+")
+hs = open("headings.txt","x")
 
 for foldername in foldernames: # loop through all the files and folders
     with open(".mapillary/logs/"+ foldername + "/sequence_process.json") as f:
